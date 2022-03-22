e7e349e01f95ff22cdcc76644ad00c1b6cfdefbd
BrunooMPS <61566542+BrunooMPS@users.noreply.github.com>
2022-03-22 18:52:17 +0000

    Click_field_Service

---
+++ click_field_service_api/click_field_handler.py
@@ -0,0 +1,26 @@
+def get_processors():
+    pass
+
+def get_memory_sticks():
+    pass
+
+def get_graphics_card():
+    pass
+
+def get_storage():
+    pass
+
+def get_power_supplies():
+    pass
+
+def get_fans():
+    pass
+
+def get_cases():
+    pass
+
+def get_coolers():
+    pass
+
+def search_part():
+    pass
