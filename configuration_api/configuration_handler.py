307dcc32efd76d7c54c1d28893b8106fe6c57a5d
Preto <preto19981@gmail.com>
2022-04-07 22:31:13 +0100

    configuration quase a funcionar (pesquisa utilizador não funciona)

--- configuration_api/configuration_handler.py
+++ configuration_api/configuration_handler.py
@@ -9,29 +9,35 @@ def create_configuration(username, body):
     print(body)
     configuration_object = {}
     configuration_object['user'] = username
-    configuration_object['configuration_id'] = uuid.uuid4()
+    configuration_object['uuid'] = str(uuid.uuid1())
     configuration_object['parts'] = body['parts']
     result = configuration_col.insert_one(configuration_object)
-    return result.inserted_id
+    print(result)
+    return configuration_object['uuid']
 
 def get_configuration_by_id(configurationId):
-    return configuration_col.find_one({'configuration_id': configurationId})
+    result = configuration_col.find_one({'uuid': configurationId})
+    print(configuration_col.find_one({'uuid': configurationId}))
+    return create_message_object(result)
 
 def update_configuration(configurationId, body):
-    current = configuration_col.find_one({'configuration_id': configurationId})
+    current = configuration_col.find_one({'uuid': configurationId})
     configuration_object = {}
     configuration_object['user'] = current['user']
-    configuration_object['configuration_id'] = current['configuration_id']
+    configuration_object['uuid'] = current['uuid']
     configuration_object['parts'] = body['parts']
-    result = configuration_col.update_one({'configuration_id': configurationId}, configuration_object)
+    result = configuration_col.update_one({'uuid': configurationId}, configuration_object)
     return current['configuration_id']
 
 def get_configuration_by_user_id(username):
+    print('here?')
+    print(username)
     search_result = configuration_col.find({'user': username})
+    print(search_result)
     conf_list = []
     for result in search_result:
         conf_list.append(create_message_object(result))
     return conf_list
 
 def create_message_object(db_object):
-    return {'user':db_object['user'], 'configuration_id': db_object['configuration_id'], 'parts': db_object['parts']}
+    return {'user':db_object['user'], 'uuid': db_object['uuid'], 'parts': db_object['parts']}
