89af9f913210d67f3f399d233953faad22e30062
Preto <preto19981@gmail.com>
2022-04-21 22:15:35 +0100

    testing server

---
+++ testing/setup.py
@@ -0,0 +1,23 @@
+import json
+import os
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+
+dataset_path = '../datasets/clickfield'
+client = MongoClient("mongodb+srv://bruno:123@cloudcomputing.2raxg.mongodb.net/CloudComputing?retryWrites=true&w=majority",server_api=ServerApi('1'))
+clickfield_db = client['clickfield_data']
+clickfield_col = clickfield_db['clickfield_data']
+
+def save_product_from_file(filepath):
+    file = open(filepath, 'r')    
+    product = json.load(file)
+    clickfield_col.insert_one(product)
+    
+def create_database():
+    file_iterator = os.scandir(dataset_path)
+    for filename in file_iterator:
+        save_product_from_file(filename)
+    
+    
+if __name__ == '__main__':
+    create_database()
---
+++ testing/testing_handler.py
@@ -0,0 +1,5 @@
+import requests
+
+def test():
+    response = requests.get()
+    return response.json
